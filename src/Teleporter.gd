8ee5a308aa0a91c19720217543330d93103caab7
jed <avisucam@gmail.com>
2024-01-22 23:33:00 +0800

    Add references to Teleporter's child components.

--- src/Teleporter.gd
+++ src/Teleporter.gd
@@ -1,2 +1,6 @@
 class_name Teleporter
 extends StaticBody3D
+
+
+@onready var collision_shape: CollisionShape3D = $CollisionShape3D
+@onready var mesh_instance: MeshInstance3D = $CollisionShape3D/MeshInstance3D
