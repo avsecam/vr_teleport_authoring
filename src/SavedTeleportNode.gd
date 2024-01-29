4afcaa6936d6b15ada595f0339104ef9d9b5254d
jed <jljocson@ateneo.edu>
2024-01-29 13:53:39 +0800

    Fix bug where Teleporters don't show on saved scenes.

--- src/SavedTeleportNode.gd
+++ src/SavedTeleportNode.gd
@@ -1,13 +1,16 @@
 extends Node3D
 
-
 @onready var mesh_instance: MeshInstance3D = $MeshInstance3D
 
 @export var panorama_texture: Texture2D
 @export var area_name: String
 
-var teleporters: Array # of Teleporters
+@export var teleporters: Array # of Teleporter PackedScenes
+@export var rotation_speed: float = 0.01
 
 
 func _ready():
 	mesh_instance.mesh.material.albedo_texture = panorama_texture
+	
+	for teleporter in teleporters:
+		mesh_instance.add_child((teleporter as PackedScene).instantiate())
