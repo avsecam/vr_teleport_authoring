f5100cfb09a2755410dfbd915b7b118a29544706
jed <jljocson@ateneo.edu>
2024-02-26 16:57:34 +0900

    Change attributes of SavedTeleportNode.

--- src/SavedTeleportNode.gd
+++ src/SavedTeleportNode.gd
@@ -1,24 +1,9 @@
-extends Node3D
+class_name SavedTeleportNode
+extends Resource
 
-@onready var mesh_instance: MeshInstance3D = $MeshInstance3D
-
-@export var panorama_texture_filename: String
 @export var area_name: String
+@export var sprite_texture_filename: String
 
-@export var teleporters: Array # of Teleporter PackedScenes
-@export var rotation_speed: float = 0.01
-
-
-func _ready():
-	mesh_instance.mesh.material.albedo_texture = (load(panorama_texture_filename) as Texture2D)
-	
-	for teleporter in teleporters:
-		mesh_instance.add_child((teleporter as PackedScene).instantiate())
-
-
-# Add a camera if you will use the following code.
-func _process(delta):
-	if Input.is_action_pressed("ui_left"):
-		$MeshInstance3D/Camera3D.rotate_y(rotation_speed)
-	if Input.is_action_pressed("ui_right"):
-		$MeshInstance3D/Camera3D.rotate_y(-rotation_speed)
+# these arrays' elements should correspond to each other
+@export var teleport_connections_names: Array # of SavedTeleportNode names
+@export var teleporters: Array # of positions
