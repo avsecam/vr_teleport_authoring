c694aea4d70acd197bed18751616a8c7e2c54415
jed <avisucam@gmail.com>
2024-03-14 23:25:55 +0800

    Add signal for switching authoring subsystem.

--- Events.gd
+++ Events.gd
@@ -1,6 +1,6 @@
 extends Node
 
-
+# Teleport Author
 signal teleport_node_selected(node: TeleportNode)
 signal teleport_node_drag_started(node: TeleportNode)
 signal teleport_node_add_requested(node: TeleportNode)
@@ -15,6 +15,11 @@ signal teleporter_add_requested(node: TeleportNode, teleporter: Teleporter)
 
 signal connection_entry_select_requested(entry: ConnectionEntry)
 signal connection_entry_delete_requested(entry: ConnectionEntry)
+
+# Event Author
+
+# Common
+signal switch_requested()
 
 signal export_requested()
 signal save_requested()
