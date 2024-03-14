a240b93e9c3e7fbb9bc3ec0a368700e37a57cc43
jed <avisucam@gmail.com>
2024-03-14 23:30:46 +0800

    Add condition for showing subsystem-specific buttons.

--- src/UI.gd
+++ src/UI.gd
@@ -1,7 +1,6 @@
 extends Control
 
-
-const connection_entry: PackedScene = preload("res://src/ConnectionEntry.tscn")
+const connection_entry: PackedScene = preload ("res://src/ConnectionEntry.tscn")
 
 @onready var project_title: Label = $TopUI/ProjectTitle
 
@@ -9,21 +8,34 @@ const connection_entry: PackedScene = preload("res://src/ConnectionEntry.tscn")
 
 @onready var file_dialog: FileDialog = $FileDialog
 
-@onready var authoring: TeleportAuthor = %TeleportAuthor
-@onready var add_button: Button = $Container/HBoxContainer/VBoxContainer/Add
-@onready var delete_button: Button = $Container/HBoxContainer/VBoxContainer/Delete
-@onready var edit_button: Button = $Container/HBoxContainer/Edit
-@onready var enter_button: Button = $Container/HBoxContainer/Enter
+@onready var teleport_author: TeleportAuthor = %TeleportAuthor
+
+@onready var switch_button: Button = $Container/HBoxContainer/Switch
+
+@onready var add_node_button: Button = $Container/HBoxContainer/VBoxContainer/AddNode
+@onready var delete_node_button: Button = $Container/HBoxContainer/VBoxContainer/DeleteNode
+@onready var edit_connections_button: Button = $Container/HBoxContainer/EditConnections
+@onready var enter_node_button: Button = $Container/HBoxContainer/EnterNode
+
+@onready var add_object_button: Button = $Container/HBoxContainer/VBoxContainer/AddObject
+@onready var delete_object_button: Button = $Container/HBoxContainer/VBoxContainer/DeleteObject
+
 @onready var export_button: Button = $Container/HBoxContainer/Export
 @onready var save_button: Button = $Container/HBoxContainer/Save
 @onready var load_button: Button = $Container/HBoxContainer/Load
 
-
 func _ready():
-	add_button.pressed.connect(_on_add_button_pressed)
-	delete_button.pressed.connect(_on_delete_button_pressed)
-	edit_button.pressed.connect(_on_edit_button_pressed)
-	enter_button.pressed.connect(_on_enter_button_pressed)
+	# Teleport Author buttons
+	add_node_button.pressed.connect(_on_add_node_button_pressed)
+	delete_node_button.pressed.connect(_on_delete_node_button_pressed)
+	edit_connections_button.pressed.connect(_on_edit_connections_button_pressed)
+	enter_node_button.pressed.connect(_on_enter_node_button_pressed)
+	
+	# Event Author buttons
+	
+	# Common buttons
+	switch_button.pressed.connect(_on_switch_button_pressed)
+	
 	export_button.pressed.connect(_on_export_button_pressed)
 	save_button.pressed.connect(_on_save_button_pressed)
 	load_button.pressed.connect(_on_load_button_pressed)
@@ -38,34 +50,45 @@ func _ready():
 	Events.connection_entry_select_requested.connect(_on_connection_entry_select_requested)
 	Events.connection_entry_delete_requested.connect(_on_connection_entry_delete_requested)
 
-
-func _process(delta):
+func _process(_delta):
+	if State.active_authoring == State.ActiveAuthoring.Teleport:
+		# Hide Event Author buttons
+		for button: Button in get_tree().get_nodes_in_group("EventAuthorButtons"):
+			button.visible = false
+		for button: Button in get_tree().get_nodes_in_group("TeleportAuthorButtons"):
+			button.visible = true
+		
+		# Add button rendering
+		add_node_button.disabled = teleport_author.in_edit_node_mode or teleport_author.in_edit_connections_mode
+		
+		# Delete button rendering
+		if teleport_author.child_selected() == null \
+			or teleport_author.in_edit_connections_mode \
+			or teleport_author.in_edit_node_mode:
+			delete_node_button.disabled = true
+		else:
+			delete_node_button.disabled = false
+		
+		# Edit button rendering
+		edit_connections_button.disabled = teleport_author.child_selected() == null or teleport_author.in_edit_node_mode
+		edit_connections_button.text = "CONFIRM" if teleport_author.in_edit_connections_mode else "EDIT CONNECTIONS"
+		
+		# Enter button rendering
+		enter_node_button.disabled = teleport_author.child_selected() == null or teleport_author.in_edit_connections_mode
+		enter_node_button.text = "CONFIRM" if teleport_author.in_edit_node_mode else "ENTER NODE"
+		
+		# Connection list rendering
+		connection_list.visible = teleport_author.in_edit_node_mode
+		
+		# Node viewer actions rendering
+	#	if teleport_author.in_edit_node_mode: blablabla
 	
-	# Add button rendering
-	add_button.disabled = authoring.in_edit_node_mode or authoring.in_edit_connections_mode
-	
-	# Delete button rendering
-	if authoring.child_selected() == null \
-		or authoring.in_edit_connections_mode \
-		or authoring.in_edit_node_mode:
-		delete_button.disabled = true
 	else:
-		delete_button.disabled = false
-	
-	# Edit button rendering
-	edit_button.disabled = authoring.child_selected() == null or authoring.in_edit_node_mode
-	edit_button.text = "CONFIRM" if authoring.in_edit_connections_mode else "EDIT CONNECTIONS"
-	
-	# Enter button rendering
-	enter_button.disabled = authoring.child_selected() == null or authoring.in_edit_connections_mode
-	enter_button.text = "CONFIRM" if authoring.in_edit_node_mode else "ENTER NODE"
-	
-	# Connection list rendering
-	connection_list.visible = authoring.in_edit_node_mode
-	
-	# Node viewer actions rendering
-#	if authoring.in_edit_node_mode: blablabla
-
+		# Hide Teleport Author buttons
+		for button: Button in get_tree().get_nodes_in_group("TeleportAuthorButtons"):
+			button.visible = false
+		for button: Button in get_tree().get_nodes_in_group("EventAuthorButtons"):
+			button.visible = true
 
 func update_connection_list(node: TeleportNode):
 	# Make number of connection entries equal the number of connections
@@ -93,7 +116,6 @@ func update_connection_list(node: TeleportNode):
 				if teleporter.teleport_location == get_node(connection):
 					entry.teleporter = teleporter
 
-
 func focus_default_connection_entry():
 	if connection_list.get_child_count() > 0:
 		# Focus first entry that has no teleporter
@@ -108,14 +130,12 @@ func focus_default_connection_entry():
 		else:
 			focus_connection_entry(connection_list.get_child(0))
 
-
 func focus_connection_entry(entry: ConnectionEntry):
 	for connection in connection_list.get_children():
 		connection.focused = false
 	
 	var entry_to_focus: ConnectionEntry = connection_list.get_child(connection_list.get_children().find(entry))
 	entry_to_focus.focused = true
-
 
 func get_focused_connection_entry():
 	for connection in connection_list.get_children():
@@ -124,12 +144,10 @@ func get_focused_connection_entry():
 	
 	return null
 
-
-func _on_add_button_pressed():
+func _on_add_node_button_pressed():
 	file_dialog.file_mode = FileDialog.FILE_MODE_OPEN_FILE
 	file_dialog.current_dir = ProjectSettings.globalize_path("res://")
 	file_dialog.show()
-
 
 func _on_file_dialog_file_selected(path: String):
 	var teleport_node: TeleportNode = load("res://src/TeleportNode.tscn").instantiate()
@@ -140,40 +158,35 @@ func _on_file_dialog_file_selected(path: String):
 	
 	Events.teleport_node_add_requested.emit(teleport_node)
 
-
 func _on_file_dialog_dir_selected(path: String):
 	project_title.text = path.get_file()
 	Events.load_requested.emit(path)
 
-
-func _on_delete_button_pressed():
-	var selected_child: TeleportNode = authoring.child_selected()
+func _on_delete_node_button_pressed():
+	var selected_child: TeleportNode = teleport_author.child_selected()
 	Events.teleport_node_delete_requested.emit(selected_child)
 
-
-func _on_edit_button_pressed():
-	var authoring: TeleportAuthor = authoring
-	var selected_child: TeleportNode = authoring.child_selected()
+func _on_edit_connections_button_pressed():
+	var selected_child: TeleportNode = teleport_author.child_selected()
 	
-	if authoring.in_edit_connections_mode:
+	if teleport_author.in_edit_connections_mode:
 		Events.teleport_node_edit_confirm_requested.emit(selected_child)
 	else:
 		Events.teleport_node_edit_requested.emit(selected_child)
 
+func _on_switch_button_pressed():
+	Events.switch_requested.emit()
 
-func _on_enter_button_pressed():
-	var authoring: TeleportAuthor = authoring
-	var selected_child: TeleportNode = authoring.child_selected()
+func _on_enter_node_button_pressed():
+	var selected_child: TeleportNode = teleport_author.child_selected()
 	
-	if authoring.in_edit_node_mode:
+	if teleport_author.in_edit_node_mode:
 		Events.teleport_node_exit_requested.emit(selected_child)
 	else:
 		Events.teleport_node_enter_requested.emit(selected_child)
 
-
 func _on_export_button_pressed():
 	Events.export_requested.emit()
-
 
 func _on_save_button_pressed():
 	save_button.text = "..."
@@ -181,25 +194,21 @@ func _on_save_button_pressed():
 	await Signal(Events.save_finished)
 	save_button.text = "SAVE"
 
-
 func _on_load_button_pressed():
 	file_dialog.file_mode = FileDialog.FILE_MODE_OPEN_DIR
 	file_dialog.current_dir = ProjectSettings.globalize_path("user://")
 	file_dialog.show()
-
 
 func _on_teleport_node_enter_requested(node: TeleportNode):
 	update_connection_list(node)
 	focus_default_connection_entry()
 	connection_list.visible = true
 
-
 func _on_teleport_node_exit_requested(_node):
 	for child in connection_list.get_children():
 		child.queue_free()
 	
 	connection_list.visible = false
-
 
 func _on_teleporter_add_requested(node: TeleportNode, teleporter: Teleporter):
 	var focused_entry: ConnectionEntry = get_focused_connection_entry()
@@ -210,10 +219,8 @@ func _on_teleporter_add_requested(node: TeleportNode, teleporter: Teleporter):
 	update_connection_list(node)
 	focus_default_connection_entry()
 
-
 func _on_connection_entry_select_requested(entry: ConnectionEntry):
 	focus_connection_entry(entry)
-
 
 func _on_connection_entry_delete_requested(entry: ConnectionEntry):
 	pass
