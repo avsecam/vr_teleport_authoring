c0fb64bda34521200e5ff1f36db2d535a8d4630b
jed <avisucam@gmail.com>
2024-06-09 21:37:41 +0800

    Adjust text display of TriggerEntry.

--- src/events/TriggerEntry.gd
+++ src/events/TriggerEntry.gd
@@ -13,6 +13,10 @@ func set_trigger_name(new_name: String):
 
 func _on_StateButton_pressed():
 	Events.trigger_toggle_requested.emit(self.trigger_name)
+	if EventFlags.data[self.trigger_name]:
+		$StateButton.text = "FALSE"
+	else:
+		$StateButton.text = "TRUE"
 
 func _on_DeleteButton_pressed():
 	Events.trigger_delete_requested.emit(self.trigger_name)
