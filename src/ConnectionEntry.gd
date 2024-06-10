bda5ec345b620fe181e532cdfb90f134deac6a63
jed <avisucam@gmail.com>
2024-06-10 23:28:31 +0800

    Add button to show LocksSetter from ConnectionEntry.

--- src/ConnectionEntry.gd
+++ src/ConnectionEntry.gd
@@ -9,20 +9,18 @@ const unfocused_color := Color(1, 1, 1)
 @export var teleporter: Teleporter
 @export var connected_to: TeleportNode:
 	set(value):
-		area_name_button.text = value.area_name
+		$AreaName.text = value.area_name
 		connected_to = value
 
-@onready var area_name_button: Button = $AreaName
-@onready var delete_button: Button = $Delete
-
 func _ready():
-	area_name_button.pressed.connect(_on_area_name_button_pressed)
-	delete_button.pressed.connect(_on_delete_button_pressed)
+	$AreaName.pressed.connect(_on_area_name_button_pressed)
+	$Delete.pressed.connect(_on_delete_button_pressed)
+	$Locks.pressed.connect(_on_locks_button_pressed)
 	
-	area_name_button.text = connected_to.area_name if connected_to else ""
+	$AreaName.text = connected_to.area_name if connected_to else ""
 
 func _process(_delta):
-	delete_button.visible = teleporter != null
+	$Delete.visible = teleporter != null
 	modulate = focused_color if focused else unfocused_color
 
 func _on_area_name_button_pressed():
@@ -30,3 +28,9 @@ func _on_area_name_button_pressed():
 
 func _on_delete_button_pressed():
 	Events.connection_entry_delete_requested.emit(self)
+
+func _on_locks_button_pressed():
+	if teleporter:
+		Events.locks_toggle_requested.emit(teleporter)
+	else:
+		push_error("ConnectionEntry has no teleporter set.")
