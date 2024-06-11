58ca0d31a8c25f15dbbb823b6364e188e8fb4924
jed <avisucam@gmail.com>
2024-06-11 13:36:00 +0800

    Change ConnectButton to CheckBox.

--- src/events/TriggerEntrySetter.gd
+++ src/events/TriggerEntrySetter.gd
@@ -10,14 +10,14 @@ func _ready():
 		push_error("Teleporter not set.")
 		return
 	
-	$ConnectButton.pressed.connect(_on_ConnectButton_pressed)
+	($ConnectButton as CheckBox).toggled.connect(_on_ConnectButton_toggled)
 	Events.trigger_toggle_connection_requested.connect(_on_trigger_toggle_connection_requested)
 
 func _process(_delta):
 	if connected:
-		$ConnectButton.text = "O"
+		($ConnectButton as CheckBox).set_pressed_no_signal(true)
 	else:
-		$ConnectButton.text = "X"
+		($ConnectButton as CheckBox).set_pressed_no_signal(false)
 
 func set_teleporter(val: Teleporter):
 	teleporter = val
@@ -28,7 +28,7 @@ func set_connected(val: bool):
 func set_state(val: bool):
 	$StateButton.text = "TRUE" if val else "FALSE"
 
-func _on_ConnectButton_pressed():
+func _on_ConnectButton_toggled(_t):
 	Events.trigger_toggle_connection_requested.emit(trigger_name, teleporter)
 
 func _on_trigger_toggle_connection_requested(_trigger_name: String, _teleporter: Teleporter):
