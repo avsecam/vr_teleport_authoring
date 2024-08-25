0f4cac24755dc9cae1fef6a2649077c785e39742
jed <avisucam@gmail.com>
2024-08-25 15:33:05 +0800

    Adjust ConnectionEntry for regular Teleporters.

--- src/ConnectionEntry.gd
+++ src/ConnectionEntry.gd
@@ -4,23 +4,25 @@ extends HBoxContainer
 const focused_color := Color(0, 1, 1)
 const unfocused_color := Color(1, 1, 1)
 
+@onready var label: Button = $AreaName
+
 @export var focused: bool = false
 
 @export var teleporter: Teleporter
 @export var connected_to: TeleportNode:
 	set(value):
-		$AreaName.text = value.area_name
+		label.text = value.area_name
 		connected_to = value
 
 func _ready():
-	$AreaName.pressed.connect(_on_area_name_button_pressed)
+	label.pressed.connect(_on_area_name_button_pressed)
 	$Delete.pressed.connect(_on_delete_button_pressed)
 	$Locks.pressed.connect(_on_locks_button_pressed)
 	
-	$AreaName.text = connected_to.area_name if connected_to else ""
+	label.text = connected_to.area_name if connected_to else ""
 
 func _process(_delta):
-	$Delete.visible = teleporter != null
+	$Delete.visible = (teleporter != null) or (not teleporter is TeleporterOutsideConnection)
 	modulate = focused_color if focused else unfocused_color
 
 func _on_area_name_button_pressed():
