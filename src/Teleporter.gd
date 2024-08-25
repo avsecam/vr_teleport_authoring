d5bc986764c582bafbee94034e66a32854fa9695
jed <avisucam@gmail.com>
2024-08-25 15:29:57 +0800

    Add Teleporter class for teleporters that are used for inter-node navigation.

--- src/Teleporter.gd
+++ src/Teleporter.gd
@@ -4,8 +4,6 @@ extends StaticBody3D
 @onready var collision_shape: CollisionShape3D = $CollisionShape3D
 @onready var mesh_instance: MeshInstance3D = $CollisionShape3D/MeshInstance3D
 
-var teleport_location: TeleportNode
-
 # name of key in EventFlags
 var locks: Array[String] = []
 
---
+++ src/TeleporterOutsideConnection.gd
@@ -0,0 +1,4 @@
+class_name TeleporterOutsideConnection
+extends Teleporter
+
+var teleport_location: TeleportNode
